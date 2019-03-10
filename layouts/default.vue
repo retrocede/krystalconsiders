89aae4ba8405cd7b71c0ddf6beeef65e9063c412
Timothy Stapleton <retrocede@outlook.com>
2019-03-09 22:46:08 -0700

    start adding netlify cms

--- layouts/default.vue
+++ layouts/default.vue
@@ -49,6 +49,17 @@
         <nuxt />
       </div>
     </section>
+    <script>
+      if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+          }
+        });
+      }
+    </script>
   </div>
 </template>
 
