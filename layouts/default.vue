dc16fc4b0d1e103f6c8f69b9c6b46b7b0dfc5633
Timothy Stapleton <retrocede@outlook.com>
2019-03-09 22:50:10 -0700

    woops, moving admin to static

--- layouts/default.vue
+++ layouts/default.vue
@@ -49,17 +49,6 @@
         <nuxt />
       </div>
     </section>
-    <script>
-      if (window.netlifyIdentity) {
-        window.netlifyIdentity.on("init", user => {
-          if (!user) {
-            window.netlifyIdentity.on("login", () => {
-              document.location.href = "/admin/";
-            });
-          }
-        });
-      }
-    </script>
   </div>
 </template>
 
